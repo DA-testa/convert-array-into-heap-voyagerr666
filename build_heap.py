2a478b50b407a1d624f915c63569438a6f06aa08
Maksims Tihomirovs <max20im14@gmail.com>
2023-03-19 14:08:05 +0200

    221RDB154 Lab.d. 3
    221RDB154 Lab.d. 3

--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,41 @@
 # python3
 
 
+def heapify(data, i, n, swaps):
+    min_index = i
+    l = 2*i + 1
+    r = 2*i + 2
+
+    if l < n and data[l] < data[min_index]:
+        min_index = l
+
+    if r < n and data[r] < data[min_index]:
+        min_index = r
+
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        heapify(data, min_index, n, swaps)
+
+
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    n = len(data)
 
+    for i in range(n//2, -1, -1):
+        heapify(data, i, n, swaps)
 
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
